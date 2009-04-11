cd69f1dfb9c37ddac49500a7a2a7503eefd7b0dc
homey1337 <homey1337@one.localdomain>
2009-04-11 11:32:08 -0500

    added support for protocol != http://

--- wb.py
+++ wb.py
@@ -29,7 +29,7 @@ class WebBrowser:
       url = 'http://www.google.com/search?q=%s' % urllib.quote_plus(url[2:])
     elif url.startswith('w '):
       url = 'http://en.wikipedia.org/w/index.php?title=Special%%3ASearch&search=%s&go=Go' % urllib.quote_plus(url[2:])
-    elif not url.startswith('http://'):
+    elif url.find('://') < 0:
       url = 'http://%s' % url
     self.webview.open(url)
 
